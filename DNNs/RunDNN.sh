09d2c2cbada5f4dd847aa7c479076898e83663d1
shehab299 <shehab299@outlook.com>
2024-12-23 22:17:10 +0200

    Add Downloading big-lama model to the bash script

--- DNNs/RunDNN.sh
+++ DNNs/RunDNN.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
 cd lama
-bash docker/2_predict_with_gpu.sh $(pwd)/big-lama $(pwd)/../in_lama $(pwd)/../out_lama device=cpu
+
+# Check if the "big-lama" folder exists
+if [ ! -d "big-lama" ]; then
+    echo "big-lama not found. Downloading..."
+    curl -LJO https://huggingface.co/smartywu/big-lama/resolve/main/big-lama.zip
+    unzip big-lama.zip
+else
+    echo "big-lama folder already exists. Skipping download."
+fi
+
+# Run the prediction script
+bash docker/2_predict_with_gpu.sh "$(pwd)/big-lama" "$(pwd)/../in_lama" "$(pwd)/../out_lama" device=cpu
+
